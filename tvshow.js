f3b48c1acdfecda3d24563962370d680afd0d90e
Shubham Poddar <shbhmpddr@gmail.com>
2023-02-09 17:28:15 +0530

    new file:   tvshow.html 	new file:   tvshow.js

---
+++ tvshow.js
@@ -0,0 +1,30 @@
+const form = document.querySelector('form');
+form.addEventListener('submit', async function(e){
+    e.preventDefault();
+    const searchTerm = form.elements.query.value;
+    form.elements.query.value = "";
+    const config = {params: {q: searchTerm}};
+    const res = await axios.get(`https://api.tvmaze.com/search/shows`,config);
+    makeImages(res.data);
+})
+
+const makeImages = shows => {
+    for(let result of shows){
+        if(result.show.image){
+            const img = document.createElement('img');
+            img.src = result.show.image.medium;
+            document.body.append(img); 
+        }
+    }
+}
+
+form.addEventListener('submit', function () {
+    deleteImgs();
+})
+
+const deleteImgs = () => {
+    const imgs = document.querySelectorAll('img');
+    for(let img of imgs){
+    img.remove();
+    }
+}
